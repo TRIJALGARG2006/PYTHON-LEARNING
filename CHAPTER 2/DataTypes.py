7a27abe86ea63be350c518545d35f9b742582ac0
TRIJALGARG <trijalgarg40@Gmail.com>
2025-09-14 03:39:32 +0530

    CHAPTER 2

---
+++ CHAPTER 2/DataTypes.py
@@ -0,0 +1,6 @@
+a = 10 # this is a integer data type 
+b = 20.5 # this is a float data type
+c = 1 + 2j # this is a complex data type
+d = "Hello, World!" # this is a string data type
+e = True # this is a boolean data type
+f = None # this is a NoneType data type
