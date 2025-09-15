a9e9ae0e3530d0f20d364d2dc7842eb8e9e96f06
TRIJALGARG <trijalgarg40@Gmail.com>
2025-09-16 04:37:23 +0530

    adding a very important comment

--- CHAPTER 3/negative_slicing.py
+++ CHAPTER 3/negative_slicing.py
@@ -3,7 +3,8 @@ name = "trijal"
 print (name[1:4])  
 # for ex 
 print (name[-5:-2])
-
+# if you wanna slice more than 5 then keep adding -1 values to the corresponding values 
+# you will get the same output
 # you can also use this output 
 print(name[:4]) # this is the same as (name[0:4])
 print(name[0:]) # same print as (name [1:6]) simply put it will print form start to full end
