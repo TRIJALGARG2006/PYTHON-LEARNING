f5498f4421b61f3973f122301a82752a643c01a3
TRIJALGARG <trijalgarg40@Gmail.com>
2025-09-16 04:23:46 +0530

    Chapter 2

--- CHAPTER 2/problem2.py
+++ CHAPTER 2/problem2.py
@@ -1,6 +1,6 @@
 #print the remainder and quotient when a is divided by b
 
-a = 39
+a = int(input("enter a number :"))
 b = 5 
 print (a%b) # prints the remainder when a is divided by b 
 print (a//b) # prints the quotient when a is divided by b 
